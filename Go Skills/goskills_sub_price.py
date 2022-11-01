33ba4e097ea9b669af05aaa81bddfa1cbef17d77
Soham Gade <soham@careervira.com>
2022-11-01 14:04:10 +0530

    New scripts - 1/11/2022

--- Go Skills/goskills_sub_price.py
+++ Go Skills/goskills_sub_price.py
@@ -1,74 +1,72 @@
 from datetime import datetime
-from locale import currency
 import scrapy
+import re
+
+class gimscrapy(scrapy.Spider):
+    name = "gim"
+    start_urls = [
+        "https://gim.ac.in/programmes/mdp"
+    ]
+
+    def parse(self, response):
+
+        dates = response.xpath('//div[@class="date"]/text()').extract()
+
+        links = response.xpath('//div[@class="body-section"]//a[@class="gim-btn-primary"]/@href').getall()
+       
+        brochure_pdfs = response.xpath('//a[@class="gim-btn-secondary"]/@href').getall()
 
 
+        for i in range(len(links)):
+            yield scrapy.Request(links[i], callback=self.parser_contents, cb_kwargs={"date":dates[i], "link":links[i], "brochure_pdfs":brochure_pdfs[i]})
 
-class MyprojectSpider(scrapy.Spider):
+    def parser_contents(self, response, date, link, brochure_pdfs):
 
-    name = "goskills" 
-    # download_delay = 3
+        title = response.xpath('//div[@class="banner-text"]//h1/text()').extract_first()
 
-    def start_requests(self):
+        regular_price = response.xpath('//span[@class="fee-price"]/text() | //th[text()="Programme Fee:"]//following::td/text()').extract_first()
+        print(regular_price)
+        currency = re.findall('[A-Z]+', regular_price)[0]
 
-        link = 'https://www.goskills.com/Bundle/Unlimited/Pricing'
-
-        yield scrapy.Request(url=link, callback=self.subscription)
-
-
-
-    def subscription(self, response):
-
-        annual_price_per_month = response.xpath("//span[@class='price annual']/span[@class='before-decimal-point']/text()").extract_first()
-        annual_currency = response.xpath("//span[@class='price annual']/span[@class='currency-symbol']/text()").extract_first()
-
-        monthly_price = response.xpath("//span[@class='price monthly']/span[@class='before-decimal-point']/text()").extract_first()
-        monthly_currency = response.xpath("//span[@class='price monthly']/span[@class='currency-symbol']/text()").extract_first()
+        regular_price = re.sub('[A-Z]+', '', regular_price)
+        regular_price = float(regular_price)
 
 
-        free_trial_price = response.xpath("//span[@class='price']/span[@class='before-decimal-point']/text()").extract_first()
-        free_trial_text = response.xpath("//span[@class='price']/following-sibling::span[@class='h5 d-block m-top-sm']/text()").extract_first()
+        year = re.findall("\d\d\d\d", date)[0]
 
-        course_page = "https://www.goskills.com/Courses"
+        date = re.sub('st|th|nd|rd', '', date)
+        dates = date.split('–')
+
+        end_date = dates[1].strip()
+        start_date = dates[0].strip()
+
+        try:
+            month = re.findall('[A-z][a-z]+', start_date)[0]
+        except:
+            month = re.findall('[A-z][a-z]+', end_date)[0]
+
+        start_date = f"{month} "+start_date+f",{year}"
+
+        try:
+            start_date = datetime.strptime(start_date, '%b %d,%Y')
+            start_date = start_date.strftime('%Y-%m-%dT00:00:00.000Z')
+        except:
+            start_date = ''
         
-        yield scrapy.Request(url=course_page, callback=self.courses, cb_kwargs={'monthly': monthly_price, 'yearly': annual_price_per_month, 'monthly_currency': monthly_currency, "annual_currency": annual_currency})
+        try:
+            end_date = datetime.strptime(end_date, '%d %b %Y')
+            end_date = end_date.strftime('%Y-%m-%dT00:00:00.000Z')
+        except:
+            end_date = ''
 
-    def courses(self, response, monthly, yearly, monthly_currency, annual_currency):
 
-        prefix = "https://www.goskills.com"
+        sale_price = regular_price
 
-        for url in response.xpath("//a[@class='bag-card-link wrapping-link']"):
-
-            link = url.xpath("./@href").extract_first()
-            course_url = prefix + link
-
-            yearly_total = float(yearly)*12
-
-            yield {
-                    "course": {
-                        "url": course_url,
-                        "regular_price": "",
-                        "sale_price": "",
-                        "subscription": {
-                            "Annual Plan": {
-                                "price_details": {
-                                    "currency": annual_currency,
-                                    "type": "",
-                                    "regular_price_monthly": yearly,
-                                    "sale_price_monthly": yearly,
-                                    "total_price": yearly_total,
-                                    "coupon": ""
-                                    }
-                            },
-                            "Monthly PLan": {
-                                "price_details": {
-                                    "currency": monthly_currency,
-                                    "type": "",
-                                    "regular_price_monthly": monthly,
-                                    "sale_price_monthly": monthly,
-                                    "coupon": ""
-                                    }
-                            }
-                        }
-                    }
-                }
+        yield {
+            "link": link,
+            "batch_start_date": start_date,
+            "batch_end_date": end_date,
+            "regular_price": regular_price,
+            "price": sale_price,
+            "currency": currency,
+        }
